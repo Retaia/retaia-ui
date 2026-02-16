4b3854f48245dec7fd89c58a34bd809915b85628
Cédric Michaux <cedric@fullfrontend.be>
2026-02-16 11:22:25 +0100

    feat(v1): adopt react-query zod and msw baseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { AppRoutes } from './routes/AppRoutes'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { appQueryClient } from './queryClient'
 
 function App() {
-  return <AppRoutes />
+  return (
+    <QueryClientProvider client={appQueryClient}>
+      <AppRoutes />
+    </QueryClientProvider>
+  )
 }
 
 export default App
