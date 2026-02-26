3c295cd47d1dc5cca171191aba5276d65d5d5588
Cédric Michaux <cedric@fullfrontend.be>
2026-02-26 22:43:38 +0100

    fix(ci): remove runtime PersistGate blocking bdd e2e startup

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,16 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { appQueryClient } from './queryClient'
 import { Provider } from 'react-redux'
 import { useMemo } from 'react'
-import { createAppPersistor, createAppStore } from './store'
-import { PersistGate } from 'redux-persist/integration/react'
+import { createAppStore } from './store'
 
 function App() {
   const store = useMemo(() => createAppStore(), [])
-  const persistor = useMemo(() => createAppPersistor(store), [store])
 
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <QueryClientProvider client={appQueryClient}>
-          <AppRoutes />
-        </QueryClientProvider>
-      </PersistGate>
+      <QueryClientProvider client={appQueryClient}>
+        <AppRoutes />
+      </QueryClientProvider>
     </Provider>
   )
 }
