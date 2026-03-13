98f801ab691e3c0337237f01f0568c1470e1beb5
Cédric Michaux <cedric@fullfrontend.be>
2026-03-13 01:52:35 +0100

    chore(ui): remove orphan modules after reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
 import { AppRoutes } from './routes/AppRoutes'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { appQueryClient } from './queryClient'
-import { Provider } from 'react-redux'
-import { useMemo } from 'react'
-import { createAppStore } from './store'
 
 function App() {
-  const store = useMemo(() => createAppStore(), [])
-
-  return (
-    <Provider store={store}>
-      <QueryClientProvider client={appQueryClient}>
-        <AppRoutes />
-      </QueryClientProvider>
-    </Provider>
-  )
+  return <AppRoutes />
 }
 
 export default App
